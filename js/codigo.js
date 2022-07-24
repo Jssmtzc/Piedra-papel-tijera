23e916c3e2a794218ca8247d8a8d7f6ed884349c
José de Jesús Martínez Cruz <jesusmtzc@outlook.es>
2022-07-24 07:25:09 -0500

    Agregar carpeta para codigo javascript

---
+++ js/codigo.js
@@ -0,0 +1,48 @@
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function eleccion(jugada) {
+  let resultado = "";
+  if (jugada == 1) {
+    resultado = " piedra 🥌🥌";
+  } else if (jugada == 2) {
+    resultado = " papel 📄📄";
+  } else if (jugada == 3) {
+    resultado = " tijera ✂✂✂";
+  } else {
+    resultado = " PERDER";
+  }
+  return resultado;
+}
+//1 es piedra,2 es papel, 3 es tijera
+let jugador = 0;
+let pc = 0;
+let triunfos = 0;
+let perdidas = 0;
+
+while (triunfos < 3 && perdidas < 3) {
+  pc = aleatorio(1, 3);
+  jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera");
+  //alert("elegiste " + jugador)
+  alert("pc elige " + eleccion(pc));
+  alert("Tu eliges " + eleccion(jugador));
+  //COMBATE
+  if (pc == jugador) {
+    alert("empate");
+  } else if (jugador == 1 && pc == 3) {
+    alert("GANASTE");
+    triunfos = triunfos + 1;
+  } else if (jugador == 2 && pc == 1) {
+    alert("GANASTE");
+    triunfos = triunfos + 1;
+  } else if (jugador == 3 && pc == 2) {
+    alert("GANASTE");
+    triunfos = triunfos + 1;
+  } else {
+    alert("PERDISTE");
+    perdidas = perdidas + 1;
+  }
+}
+
+alert("Ganaste: " + triunfos + " Perdiste : " + perdidas);
